fcbfc8cf5debdb3843351f936531f289fc2269d4
DanielVanetti <danielgonzalezpicado@gmail.com>
2024-05-26 16:42:11 -0600

    Login adn Signin finished and another changes in app routes but idk how it better

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,26 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {path:'',component: HomeComponent},
+    {path:'login',component: LoginComponent},
+    {path:'login/signup', component: SignupComponent}
 ];
+/**
+ * 
+ * const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent }
+];
+ * 
+ */
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
