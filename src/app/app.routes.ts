5dae76ce630c6d5d5d05c50dba78b2000b14b94f
Alejandro <alejandro6chaves@gmail.com>
2024-05-27 16:14:22 -0600

    facturacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ShopComponent } from './components/shop/shop.component';
+import { SaleComponent } from './components/sale/sale.component';
 
 export const routes: Routes = [
     {path:'',component: HomeComponent},
     {path:'login',component: LoginComponent},
-    {path:'sigup',component: SignupComponent},
+    {path:'signup',component: SignupComponent},
     {path:'shop',component: ShopComponent},
+    {path: 'sale',component:SaleComponent}
 ];
