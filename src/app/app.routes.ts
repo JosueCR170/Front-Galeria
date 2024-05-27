e7924aebba163be84e98e5efa9c0b5aa2b4f010d
Alejandro <alejandro6chaves@gmail.com>
2024-05-26 23:47:48 -0600

    no se algo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 export const routes: Routes = [
     {path:'',component: HomeComponent},
+    {path:'login',component: LoginComponent},
+    {path:'sigup',component: SignupComponent},
+    {path:'shop',component: ShopComponent},
 ];
