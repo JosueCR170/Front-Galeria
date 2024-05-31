3e704a127555d99adf7bd644a066e6b222f2a9aa
Alejandro <alejandro6chaves@gmail.com>
2024-05-30 18:24:22 -0600

    'actualizacion2DelDia'

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -6,16 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [CommonModule, ButtonModule, BrowserAnimationsModule],
-  templateUrl: './shop.component.html',
-  styleUrl: './shop.component.css'
+  imports: [CommonModule],
+  templateUrl:'./shop.component.html',
+  styleUrl:'./shop.component.css'
 })
 export class ShopComponent {
- 
+  selectedCategory: string = '';
 
   categorias = [
     {
-      nombre: 'Cubismo',
+      nombre: 'Cubism',
       obras: [
         { nombre: 'Obra 1', precio: 1900, imagen: '/assets/img/prueba2.jpg' },
         { nombre: 'Obra 2', precio: 1900, imagen: '/assets/img/prueba2.jpg' },
@@ -29,7 +29,7 @@ export class ShopComponent {
       ]
     },
     {
-      nombre: 'Realismo',
+      nombre: 'Impressionism',
       obras: [
         { nombre: 'Obra 1', precio: 1900, imagen: '/assets/img/prueba2.jpg' },
         { nombre: 'Obra 2', precio: 1900, imagen: '/assets/img/prueba2.jpg' },
@@ -43,4 +43,18 @@ export class ShopComponent {
     return categoria.obras.length;
   }
   cant=this.getNumeroDeObras;
+
+  // Método para manejar la selección de una categoría
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
+  selectNav(category: string) {
+    this.selectedCategory = category;
+  }
+
+  // Método para obtener las obras por categoría
+  getObrasByCategory(category: string) {
+    const categoria = this.categorias.find(cat => cat.nombre === category);
+    return categoria ? categoria.obras : [];
+  }
 }
