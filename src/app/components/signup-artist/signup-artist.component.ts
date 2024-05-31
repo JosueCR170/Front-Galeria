ffc3fe940b51c2438b9ee37affd3bf3774bdffaf
DanielVanetti <danielgonzalezpicado@gmail.com>
2024-05-30 18:39:56 -0600

    Login and Signin artist

---
+++ src/app/components/signup-artist/signup-artist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-artist',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup-artist.component.html',
+  styleUrl: './signup-artist.component.css'
+})
+export class SignupArtistComponent {
+
+}
