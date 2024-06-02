c150ae74c14a6aea4a782cf1163ace862f305c20
DanielVanetti <danielgonzalezpicado@gmail.com>
2024-06-01 22:49:43 -0600

    Sigin

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { User } from '../../models/User';
+import { UserService } from '../../services/user.service';
+import { timer } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  public status:number;
+  public user:User;
+  constructor(
+    private _userService:UserService
+  ){
+    this.status=-1;
+    this.user=new User(1,"",false,"","","","");
+  }
 
+  onSubmit(form:any){
+    this.user.tipoUsuario=false;
+    this._userService.create(this.user).subscribe({
+      next:(response)=>{
+        console.log(response);
+        if(response.status==201){
+          form.reset();            
+          this.changeStatus(0);
+        }else{
+          this.changeStatus(1);
+        }
+      },
+      error:(error:Error)=>{
+        this.changeStatus(2);
+      }
+    })
+  }
+  changeStatus(st:number){
+    this.status=st;
+    let countdown=timer(5000);
+    countdown.subscribe(n=>{
+      this.status=-1;
+    })
+  }
 }
