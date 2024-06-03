9b6514c9dbc94884a1f2dbc2c6c0b6cbc0834bc0
Alejandro <alejandro6chaves@gmail.com>
2024-06-02 19:36:04 -0600

    tabla se muestra sin edir

--- src/app/components/artista-administration/artista-administration.component.ts
+++ src/app/components/artista-administration/artista-administration.component.ts
@@ -1,20 +1,51 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ObraService } from '../../services/obra.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Obra } from '../../models/Obra';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+import { ProductService } from '../../services/productservice';
+import { Product } from '../../models/Product';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { CommonModule } from '@angular/common';
+import { TagModule } from 'primeng/tag';
+import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
-import { FilterMatchMode, PrimeNGConfig } from 'primeng/api';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-artista-administration',
   standalone: true,
-  imports: [],
+  imports: [TableModule, ToastModule,FormsModule, CommonModule, TagModule, DropdownModule, ButtonModule, InputTextModule],
   templateUrl: './artista-administration.component.html',
-  styleUrl: './artista-administration.component.css'
+  styleUrl: './artista-administration.component.css',
+  providers: [ProductService, MessageService]
 })
 export class ArtistaAdministrationComponent {
+  productDialog: boolean = false;
+
+    products!: Product[];
+
+    cantidad!: Product[];
+
+    product!: Product;
+
+    selectedProducts!: Product[] | null;
+
+    submitted: boolean = false;
+
+    statuses!: any[];
+
+    selectAll: boolean = false;
+
+    selectedProduct!: Product[];
+    totalRecords!: number;
+
+    clonedProducts: { [s: string]: Product } = {};
+  
+    editing: boolean = false;
+  /*-------*/
 
   flag: boolean = true;
   delivry: boolean = false;
@@ -25,11 +56,10 @@ export class ArtistaAdministrationComponent {
   public obra: Obra;
   selectedArtistId: number = 1;
   constructor(
+    private productService: ProductService,
     private _obraService: ObraService,
-    private _router: Router,
-    private _routes: ActivatedRoute,
-    private primengConfig: PrimeNGConfig,
-    private config: PrimeNGConfig
+    private messageService: MessageService
+    
   ) {
     this.status = -1;
     this.obra = new Obra(1, 1, "", "", "", 1, true, "", null, null, null);
@@ -37,24 +67,9 @@ export class ArtistaAdministrationComponent {
 
 
   ngOnInit(): void {
-    this.index();
-   /* this.primengConfig.ripple = true;
-    this.primengConfig.zIndex = {
-      modal: 1100,    // dialog, sidebar
-      overlay: 1000,  // dropdown, overlaypanel
-      menu: 1000,     // overlay menus
-      tooltip: 1100 }
-      this.primengConfig.filterMatchModeOptions = {
-        text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
-        numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
-        date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
-      };
-      this.config.setTranslation({
-        accept: 'Accept',
-        reject: 'Cancel',
-        //translations
-    });*/
+    this.loadProducts();
   }
+  
 
   index() {
     this._obraService.index().subscribe({
@@ -87,5 +102,47 @@ export class ArtistaAdministrationComponent {
     this.delivry = false;
     this.administration = show;
   }
+
+  /************************* */
+  onSelectionChange(value = []) {
+    this.selectAll = value.length === this.totalRecords;
+    this.selectedProduct = value;
+  }
+  
+  onSelectAllChange(event: any) {
+    const checked = event.checked;
+
+    if (checked) {
+        this.productService.getProducts().then((res) => {
+            this.selectedProduct = res;
+            this.selectAll = true;
+        });
+    } else {
+        this.selectedProduct = [];
+        this.selectAll = false;
+    }
+}
+
+
+loadProducts() {
+  this.products = this.productService.getProductsData();
+  this.totalRecords = this.products.length;
+}
+
+onRowEditInit(product: Product, index: number) {
+  this.clonedProducts[product.id as string] = { ...product };
+  this.editing = true;
+}
+
+onRowEditSave(product: Product) {
+  delete this.clonedProducts[product.id as string];
+  this.editing = false;
+}
+
+onRowEditCancel(product: Product, index: number) {
+  this.products[index] = this.clonedProducts[product.id as string];
+  delete this.clonedProducts[product.id as string];
+  this.editing = false;
+}
 
 }
