5d5b66897dddb2f8e479f291068c194ada4cdb4b
Alejandro <alejandro6chaves@gmail.com>
2024-06-02 23:12:36 -0600

    avance de admin2.0

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ObraService } from '../../services/obra.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { Obra } from '../../models/Obra';
 
 @Component({
   selector: 'app-admin',
@@ -8,24 +12,119 @@ import { Component } from '@angular/core';
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  
 
-  flag: boolean = true;
+
+
   delivry: boolean = false;
   administration: boolean = false;
+  public status: number;
+  public obra: Obra;
+  constructor(
+    private _obraService: ObraService,
+    private _router: Router,
+    private _routes: ActivatedRoute,
+    private _userService: UserService
+  ) {
+    this.status = -1;
+    this.obra = new Obra(1, 1, "", "", "", 1, true, "", null, null, null);
+  }
+
+  obras: Obra[] = [];
+  auxObras: Obra[] = [];
+  categoryExists: string[] = [];
+  selectedCategory: string = '';
+  flag: boolean = true;
+  userClick: boolean = false;
+  artistClick: boolean = false;
+  invoiceClick: boolean = false;
+  shippingClick: boolean = false;
+  workClick: boolean = false;
+  user: any;
+
+
+
+
+  ngOnInit():void {
+    // Aquí puedes llamar al método que desees que se ejecute al cargar el componente
+    this.loadLoggedUser();
+  }
+
+  loadLoggedUser(){
+    this.user = sessionStorage.getItem('identity');
+    this.user = JSON.parse(this.user);
+  }
+
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
 
   showHome(show: boolean) {
     this.flag = show;
-    this.delivry = false;
-    this.administration = false;
+    this.userClick = false;
+    this.artistClick = false;
+    this.invoiceClick = false;
+    this.shippingClick = false;
+    this.workClick = false;
   }
-  showDelivery(show: boolean) {
+
+  showUser(all: boolean) {
     this.flag = false;
-    this.delivry = show;
-    this.administration = false;
+    this.userClick = all;
+    this.artistClick = false;
+    this.invoiceClick = false;
+    this.shippingClick = false;
+    this.workClick = false;
+  }
+
+  showArtist(all: boolean) {
+    this.flag = false;
+    this.userClick = false;
+    this.artistClick = all;
+    this.invoiceClick = false;
+    this.shippingClick = false;
+    this.workClick = false;
+  }
+  showWork(all: boolean) {
+    this.flag = false;
+    this.userClick = false;
+    this.artistClick = false;
+    this.invoiceClick = false;
+    this.shippingClick = false;
+    this.workClick = true;
+  }
+  showInvoice(all: boolean) {
+    this.flag = false;
+    this.userClick = false;
+    this.artistClick = false;
+    this.invoiceClick = true;
+    this.shippingClick = false;
+    this.workClick = false;
+  }
+  showShipping(all: boolean) {
+    this.flag = false;
+    this.userClick = false;
+    this.artistClick = false;
+    this.invoiceClick = false;
+    this.shippingClick = true;
+    this.workClick = false;
   }
   adminObras(show: boolean) {
     this.flag = false;
     this.delivry = false;
     this.administration = show;
+    
   }
+  loadAdmin(){
+    this._router.navigate(['/shop'])
+    }
+    
+  redirectToLoginArtist() {
+    this._router.navigate(['/loginArtist']);
+  }
+  logOut(){
+    sessionStorage.clear();
+    this._router.navigate([''])
+  }
+
 }
--- src/app/components/sale/sale.component.ts
+++ src/app/components/sale/sale.component.ts
@@ -1,6 +1,8 @@
 import { Component, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from '../../app.component';
+import { Obra } from '../../models/Obra';
+import { ObraService } from '../../services/obra.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-sale',
@@ -15,10 +17,63 @@ export class SaleComponent {
    totalCost: string = '';
    productPrice: number = 19000;
 
-  updateShippingMethod(event: Event): void {
+
+   
+  public obra: Obra;
+  public status: number;
+
+  constructor(
+    private _obraService: ObraService,
+    private _router: Router,
+    private _routes: ActivatedRoute,
+    private _userService: UserService
+  ) {
+    this.status = -1;
+    this.obra = new Obra(1, 1, "", "", "", 1, true, "", null, null, null);
+  }
+  user: any;
+
+  ngOnInit(): void {
+    const id = this._routes.snapshot.paramMap.get('id');
+    console.log('ID obtenido de la ruta:', id); // Verificar que el ID se está obteniendo correctamente
+    if (id) {
+      this.getObra(id);
+    }
+    this.loadLoggedUser();
+  }
+     getObra(id: string): void {
+       this._obraService.getArtworkById(id).subscribe(
+         data => {
+        //console.log('Obra obtenida:', data); // Verificar los datos obtenidos
+        this.obra = data['obra'];
+        this.status = 1;
+      },
+      error => {
+        console.error('Error al obtener la obra:', error);
+        this.status = 0;
+      }
+    );
+  }
+
+  getImage(obra: Obra): string | null {
+    if (obra.imagen) {
+      // Decodificar la imagen base64 y devolverla como una URL base64
+      return 'data:image/jpeg;base64,' + obra.imagen;
+  
+    } else {
+      return null;
+    }
+  }
+  
+  
+  loadLoggedUser(){
+    this.user = sessionStorage.getItem('identity');
+    this.user = JSON.parse(this.user);
+  }
+
+    updateShippingMethod(event: Event): void {
     const selectElement = event.target as HTMLSelectElement;
     const selectedValue = selectElement.value;
-
     if (selectedValue === 'Home delivery') {
       this.shippingCost = '3.000';
       this.totalCost = (this.productPrice + 3000).toString();
