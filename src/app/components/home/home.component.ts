02cd3efc149ca22dc868ea526090f15c20b3ace8
DanielVanetti <danielgonzalezpicado@gmail.com>
2024-06-03 19:08:33 -0600

    hecho el home de img

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ObraService } from '../../services/obra.service';
+import { Obra } from '../../models/Obra';
+import { UserService } from '../../services/user.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+ 
 
 }
