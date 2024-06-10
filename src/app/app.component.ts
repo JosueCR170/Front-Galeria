646490daa4327ce529faede6655892024ee3bedc
Alejandro <alejandro6chaves@gmail.com>
2024-06-09 22:42:08 -0600

    sing up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink, TableModule],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -27,17 +27,6 @@ ngOnInit() {
     tooltip: 1100   // tooltip
 };
 
-  this.primengConfig.filterMatchModeOptions = {
-    text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
-    numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
-    date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
-};
-
-this.primengConfig.setTranslation({
-  accept: 'Accept',
-  reject: 'Cancel',
-  //translations
-});
 
 }
 }
