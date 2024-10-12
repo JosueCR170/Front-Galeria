7603c4068038634dc083b2c79584803fe3939445
Alejandro <alejandro6chaves@gmail.com>
2024-10-11 20:48:56 -0600

    SPA cursos

---
+++ src/app/components/categorie-course/categorie-course.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorie-course',
+  standalone: true,
+  imports: [],
+  templateUrl: './categorie-course.component.html',
+  styleUrl: './categorie-course.component.css'
+})
+export class CategorieCourseComponent {
+
+}
