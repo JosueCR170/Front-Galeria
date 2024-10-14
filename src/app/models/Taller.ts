1c1dd43363addc8383ed2da156f6b93a9d60a2a5
Pipe1844 <pipevifa@gmail.com>
2024-10-13 22:29:57 -0600

    Services oferta,matricula,taller

--- src/app/models/Taller.ts
+++ src/app/models/Taller.ts
@@ -1,6 +1,7 @@
 export class Taller{
     constructor(
         public id:number,
+        public idArtista:number,
         public nombre:string,
         public descripcion:string,
         public duracion:number,
