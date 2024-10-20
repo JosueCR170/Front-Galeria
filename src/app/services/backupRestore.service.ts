f8d3527e30e4ee4613ed274c1a21376705a29704
JosueCR170 <josolorzano4@gmail.com>
2024-10-20 16:57:31 -0600

    service para backup

---
+++ src/app/services/backupRestore.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { server } from "./global";
+import { BehaviorSubject, Observable, tap } from "rxjs";
+import { BackupRestoreBD } from "../models/BackupRestoreBD";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ArtistService{
+    private urlAPI:string;
+    private backupRestoreSubject = new BehaviorSubject<any>(null);
+    public backupRestore$ = this.backupRestoreSubject.asObservable();
+    constructor(
+        private _http:HttpClient
+    ){
+        this.urlAPI = server.url;
+    }
+
+
+    restoreBD(backupRestore:BackupRestoreBD): Observable<any> {
+        console.log(backupRestore);
+        let backupRestoreJson=JSON.stringify(backupRestore);
+        let params='data='+backupRestoreJson;
+        let headers;
+        let bearertoken = sessionStorage.getItem('token');
+        if (bearertoken){
+            headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded').set('bearertoken', bearertoken);
+        } else {
+            headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        }
+        let options = {
+            headers
+        };
+        return this._http.put(`${this.urlAPI}adminBD/restore`, params, options);
+    }
+
+    backupBD(backupRestore:BackupRestoreBD): Observable<any> {
+        console.log(backupRestore);
+        let backupRestoreJson=JSON.stringify(backupRestore);
+        let params='data='+backupRestoreJson;
+        let headers;
+        let bearertoken = sessionStorage.getItem('token');
+        if (bearertoken){
+            headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded').set('bearertoken', bearertoken);
+        } else {
+            headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+        }
+        let options = {
+            headers
+        };
+        return this._http.put(`${this.urlAPI}adminBD/backup`, params, options);
+    }
+
+
+}
